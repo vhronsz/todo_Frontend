5057db28b249c2ec9d063161e9dfe8823923d9fa
Ryan Sanjaya <ryansanjaya290799@gmail.com>
2021-11-11 12:13:30 +0700

    Testing Axios

--- src/Page/Login.jsx
+++ src/Page/Login.jsx
@@ -1,11 +1,34 @@
 import React from "react";
+import axios from "axios";
+
+const BASE_URL = "http://localhost:3000";
 
 class Login extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {content: "Hello"}
 
+    }
+    componentDidMount(){
+        axios({
+                method: 'get',
+                url: `${BASE_URL}`,
+                headers: {'Access-Control-Allow-Origin': '*','Content-Type': 'application/json'},
+            }).then(
+                (res)=>{
+                    console.log(res);
+                    this.setState({
+                        content : res.data.data
+                    });
+                }
+            ).catch((err)=>{
+                console.log(err)
+            });
+    }
     render(){
         return(
             <div>
-                Hello
+                {this.state.content}
             </div>
         )
     }
